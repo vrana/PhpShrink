4d12c4a04980b9e67612d9fc48291d3ca8969bc4
Jakub Vrana <jakub@vrana.cz>
2025-03-16 22:24:26 +0100

    Runner: concat files before shrinking

--- run.php
+++ run.php
@@ -6,6 +6,14 @@ array_shift($argv);
 if (!$argv) {
 	$argv = array("php://stdin");
 }
-foreach ($argv as $filename) {
-	echo phpShrink(file_get_contents($filename)) . "\n";
+$input = "";
+foreach ($argv as $i => $filename) {
+	$file = file_get_contents($filename);
+	$tokens = token_get_all($file);
+	$token = end($tokens);
+	$input .= $file;
+	if (isset($argv[$i+1]) && !(is_array($token) && in_array($token[0], array(T_CLOSE_TAG, T_INLINE_HTML)))) {
+		$input .= "\n?>";
+	}
 }
+echo phpShrink($input) . "\n";
