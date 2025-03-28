0245ae4ea92f7de28c0b5633fc8ed691dbb25992
Jakub Vrana <jakub@vrana.cz>
2025-03-28 10:28:51 +0100

    Add function for stripping types

--- tests/basic.php
+++ tests/basic.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 include __DIR__ . "/../phpShrink.php";
 
---
+++ tests/types.php
@@ -0,0 +1,36 @@
+#!/usr/bin/env php
+<?php
+include __DIR__ . "/../phpShrink.php";
+
+function check($code, $expected) {
+	$stripped = stripTypes($code);
+	if ($stripped != $expected) {
+		$backtrace = debug_backtrace();
+		$backtrace = $backtrace[0];
+		echo "$backtrace[file]:$backtrace[line]:$stripped\n";
+	}
+}
+
+// bugs
+check('"var int $a";', '"var $a";');
+
+check('function f(int $a) {}', 'function f($a) {}');
+check('function f(int $a);', 'function f($a);');
+check('function f(array $a, string $b);', 'function f(array $a, $b);');
+check('function f(array $a);', 'function f(array $a);');
+check('function f(\stdClass $a);', 'function f(\stdClass $a);');
+check('function f(Custom $a);', 'function f(Custom $a);');
+check('function f(?Custom $a);', 'function f($a);');
+check('function f($a);', 'function f($a);');
+check('function f(int &$a);', 'function f(&$a);');
+check('function f($a = array());', 'function f($a = array());');
+check('function f(): array;', 'function f();');
+check('function f(): int;', 'function f();');
+check('function (): int {}', 'function () {}');
+check('function (): ?int {}', 'function () {}');
+check('public ?array $a;', 'public $a;');
+check('public stdClass $a;', 'public $a;');
+check('public \stdClass $a;', 'public $a;');
+
+// not supported
+check('var A|B $a;', 'var A|B $a;');
